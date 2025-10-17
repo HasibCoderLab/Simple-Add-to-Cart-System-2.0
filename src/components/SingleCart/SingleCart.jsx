7047e4f5542ac546f6d3ac526ba1b33395da2c3d
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-17 21:43:21 +0600

    update 'SimgleCart Compo./.'

--- src/components/SingleCart/SingleCart.jsx
+++ src/components/SingleCart/SingleCart.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 const SingleCart = ({singleCart}) => {
-    const {name , price, category } = singleCart;
+    const {name , price, category,img } = singleCart;
     console.log(singleCart);
     
   return (
     <div>
         <h2>Name :{name}</h2>
-        <h2>Name :{name}</h2>
-        <h2>Name :{name}</h2>
-        <h2>Name :{name}</h2>
+        <p>Price :{price}</p>
+        <h4>Category :{category}</h4>
+        <img src={img} alt="" />
     </div>
   )
 }
