cbedb468fcd64f6e6aac26a3441a835f02cd77bb
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-17 12:23:44 +0600

    flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
+import Placement from './components/Placement/Placement'
 import Products from './components/Products/Products'
 
 function App() {
 
 
   return (
-   <div>
+   <div className='flex gap-5'>
     <Products></Products>
+    <Placement></Placement>
    </div>
   )
 }
